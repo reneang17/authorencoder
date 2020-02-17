e6f2389f105c9f48ac3633f4fe00b1855c59264d
rene angele <reneang17@gmail.com>
2020-02-16 21:37:20 -0500

    removed duplicate triplet loss module

--- src/siamese_triplet/triplet_loss/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-from .losses import OnlineTripletLoss
-from .utils import AllTripletSelector,HardestNegativeTripletSelector, RandomNegativeTripletSelector,\
-    SemihardNegativeTripletSelector, extract_embeddings # Strategies for selecting triplets within a minibatch
-from .metrics import AverageNonzeroTripletsMetric
-from .train import simplified_fit
-from .datasets import BalancedBatchSampler, data_to_Iterator
-from .train import test_epoch
